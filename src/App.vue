740cf3cdd162709a74d7b84eaad71f6e4116e7d7
WgetDc <ingcorellana@gmail.com>
2020-05-10 01:08:25 -0400

    Header navbar 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <px-header/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    PxHeader    
   }
 };
 </script>
@@ -24,5 +23,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+*{
+  background: #0d1219;
+  font-family: 'Ubuntu', sans-serif;
 }
 </style>
---
+++ src/components/PxHeader.vue
@@ -0,0 +1,81 @@
+<template>
+<header>
+    <nav class="navegacion">
+        <div>
+            <img src="@/assets/logo.png" alt="WgetDc">
+            <span class="nombre">Carlos Orellana</span>
+            <span class="subnombre">Ingeniero Informático - Docente - Profesor de Matemática</span>
+        </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><i class="fa fa-search buscar"></i></li>
+        </ul>
+        <hr>
+    </nav>
+</header>
+    
+</template>
+
+<style scoped>
+
+img{
+    width: 50px;
+    margin-left: 40px;
+}
+
+ul, li{
+    display: flex;
+    margin-right: 20px;
+    margin-bottom: 10px;
+    
+}
+
+ul li:last-child {
+    margin-left: auto;
+}
+
+
+a{
+    text-decoration: none;
+    color: #cfcfcf;
+    font-size: 16px;
+}
+
+nav{
+    margin-left: 200px;
+    margin-right: 200px;
+}
+
+hr{
+    margin-left: 40px;
+}
+
+i{
+    color: #ffffff;
+}
+
+
+
+.nombre{
+    color: #ffffff;
+    margin-left: 12px;
+    position: absolute;
+    font-size: 22px;
+    font-weight: bold;
+}
+
+.subnombre{
+    color: #cfcfcf;
+    margin-left: 12px;
+}
+
+.push {
+    margin-left: auto;
+}
+
+
+
+
+</style>
