17199ad71dc365f62ce94c975ec92e678ce7be01
WgetDc <ingcorellana@gmail.com>
2020-05-14 01:12:06 -0400

    Tarjetitas con grid listeilors

--- src/components/PxBody.vue
+++ src/components/PxBody.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <div class="contenedor">
-            <div class="contenedor-1 contenedor-div">                
-                <img src="https://i.picsum.photos/id/1060/536/354.jpg" alt="">                
+            <div class="contenedor-1 contenedor-div">                                
+                <img src="https://www.w3schools.com/css/img_5terre_wide.jpg" alt="">      
+                <h3>Título</h3>          
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, dolorum.</p>
             </div>
-            <div class="contenedor-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto voluptatibus quas autem earum deleniti neque enim incidunt, nihil eaque iusto.</div>
+            <div class="contenedor-2 contenedor-div">                                
+                <img src="https://www.w3schools.com/css/img_5terre_wide.jpg" alt="">      
+                <h3>Título</h3>          
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, dolorum.</p>
+            </div>
         </div>
     </div>
 </template>
@@ -19,7 +24,7 @@
 .contenedor{
     display: grid;
     grid-template-columns: repeat(6, 1fr);
-    gap: 1em;
+    gap: 4em;
 }
 
 .contenedor-div{
@@ -33,20 +38,27 @@
     border: 1px solid yellow;
 }
 
-.contenedor p, img{
+.contenedor img{
     background: #141F2D ;
+    max-width: 100%;
+    height: auto;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
 }
 
-.contenedor p{
+.contenedor p, h3{
     margin: 10px;
+    background: #141F2D ;
+    max-width: 95%;
+    max-height: auto;
 }
 
 
 .contenedor-1{
-    grid-column: 2/5;
+    grid-column: 2/4;
 }
 
 .contenedor-2{
-    grid-column: 5/7;
+    grid-column: 4/6;
 }
 </style>
--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -10,9 +10,15 @@
             <li><a href="#">Home</a></li>
             <li><a href="#">Contact</a></li>
             <li><a href="#">Projects</a></li>
-            <li><i class="fa fa-search buscar"></i></li>
+            <li>
+                <a href="https://twitter.com/WgetDc" target="_blank">
+                    <i class="fab fa-twitter"></i>
+                </a>                 
+                <a href="https://www.instagram.com/wgetdc/" target="_blank">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </li>
         </ul>
-        <hr>
     </nav>
 </header>
     
@@ -37,6 +43,7 @@ ul li:last-child {
 }
 
 
+
 a{
     text-decoration: none;
     color: #cfcfcf;
@@ -47,14 +54,17 @@ nav{
     margin-top: 50px;
     margin-left: 300px;
     margin-right: 300px;
+    margin-bottom: 30px;
 }
 
 hr{
     margin-left: 40px;
+    margin-bottom: 40px;
 }
 
 i{
     color: #ffffff;
+    margin-left: 10px;
 }
 
 
